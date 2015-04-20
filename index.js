78317edf2c8844d8942fbb670e070422cc286f33
John J. Glynn IV <jglynn@factset.com>
2015-04-20 12:58:57 -0400

    Update 2015-04-20T16:58:56.255Z

--- index.js
+++
@@ -1,6 +0,0 @@
-(function() {
-  angular.module("mtg", ["ui.router", "angular-gestures", "sprintf"]);
-
-}).call(this);
-
-//# sourceMappingURL=index.js.map
--- mtg/mtg-settings-controller.js
+++
@@ -1,6 +0,0 @@
-(function() {
-  angular.module("mtg").controller("mtgSettingsController", function($scope) {});
-
-}).call(this);
-
-//# sourceMappingURL=../mtg/mtg-settings-controller.js.map
